6bf0a1f04299e3807418d640dad38eb039f10146
dkawula <dkawula@triconelite.com>
2018-02-10 11:05:18 -0700

    Updated to cleanup final Routing Configs

--- BigDemo_Insider_Routing_WSUS_V1.ps1
+++ BigDemo_Insider_Routing_WSUS_V1.ps1
@@ -739,17 +739,19 @@ Function Install-RRAS{
     new-netroute -InterfaceAlias "Ethernet 2" -NextHop $newroute  -DestinationPrefix '0.0.0.0/0' -verbose
     $null = Get-NetAdapter | where name -EQ "Ethernet" | Rename-NetAdapter -NewName CorpNet
     $null = Get-NetAdapter | where name -EQ "Ethernet 2" | Rename-NetAdapter -NewName Internet
-
+    Write-Output -InputObject "[$($VMName)]:: Installing RRAS"
     $null = Install-WindowsFeature -Name RemoteAccess,Routing,RSAT-RemoteAccess-Mgmt 
     #$null =  Stop-Service -Name WDSServer -ErrorAction SilentlyContinue
     #$null = Set-Service -Name WDSServer -StartupType Disabled -ErrorAction SilentlyContinue
 
     $ExternalInterface="Internet"
     $InternalInterface="CorpNet"
-
+    Write-Output -InputObject "[$($VMName)]:: Coniguring RRAS - Adding Internal and External Adapters"
     $null = Start-Process -Wait:$true -FilePath "netsh" -ArgumentList "ras set conf ENABLED"
     $null = Set-Service -Name RemoteAccess -StartupType Automatic
     $null = Start-Service -Name RemoteAccess
+
+     Write-Output -InputObject "[$($VMName)]:: Configuring NAT - Lab is now Internet Enabled"
     $null = Start-Process -Wait:$true -FilePath "netsh" -ArgumentList "routing ip nat install"
     $null = Start-Process -Wait:$true -FilePath "netsh" -ArgumentList "routing ip nat add interface ""CorpNet"""
     $null = Test-NetConnection 192.168.10.1
@@ -759,6 +761,8 @@ Function Install-RRAS{
     $null = Test-NetConnection 192.168.10.1
    # $null = Test-NetConnection $($Subnet)1
     $null = Test-NetConnection 4.2.2.2
+     Write-Output -InputObject "[$($VMName)]:: Disable FireWall"
+    $null = cmd.exe /c "netsh firewall set opmode disable"
       
     
     }
@@ -1104,6 +1108,7 @@ Invoke-Command -VMName $VMName -Credential $localCred {
 } -ArgumentList $VMName, $domainCred, $domainName
 
 Install-RRAS -VMName $VMName
+Restart-DemoVM $VMName 
 
 $VMName = 'VMM01'
 $GuestOSName = 'VMM01'
